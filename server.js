4911bc4250b7e880b889842798a39b910249eef4
joseph inziano <joseph inziano@WFPKENRB-FIN007.global.wfp.org>
2019-04-16 14:17:17 +0300

    update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/pichr'));
+app.use(express.static('./dist/pichr'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/pichr/index.html'));
+res.sendFile(path.join(__dirname, '/dist/pichr/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
