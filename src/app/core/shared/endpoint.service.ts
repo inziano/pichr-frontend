698445a31715c47f606debb20e40101bae956d86
joseph inziano <joseph inziano@WFPKENRB-FIN007.global.wfp.org>
2019-04-19 13:10:10 +0300

    Updated environment options and variables

--- src/app/core/shared/endpoint.service.ts
+++ src/app/core/shared/endpoint.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
 import { Headers, RequestOptions } from '@angular/http';
 
 // Minio
@@ -10,7 +11,7 @@ import * as aws from 'aws-sdk';
 export class EndpointsService {
 
     // This service connects the api.
-    public baseUrl = 'http://localhost:8000/';
+    public baseUrl = environment.api_url || 'http://localhost:8000/';
     public apiUrl = 'api/';
     public fullUrl = this.baseUrl + this.apiUrl;
 
@@ -28,7 +29,7 @@ export class EndpointsService {
     public bucket = new aws.S3 ( {
         accessKeyId: 'T3P6VYMYEUG0D9YLOZRY',
         secretAccessKey: 'pkFy5z4FUR0XHaqqS4voJbLXsFtXT884SwD63bkf',
-        endpoint: 'http://127.0.0.1:9000',
+        endpoint: environment.aws_endpoint || 'http://127.0.0.1:9000',
         s3ForcePathStyle: true, // needed with minio?
         signatureVersion: 'v4'
     });
