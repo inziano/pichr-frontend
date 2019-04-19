4789a4559ddced7e160caa48b11ae3b3022c5cca
joseph inziano <joseph inziano@WFPKENRB-FIN007.global.wfp.org>
2019-04-19 14:54:31 +0300

    update

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    api_url: 'ec2-13-234-204-8.ap-south-1.compute.amazonaws.com/',
+    api_url: 'https://ec2-13-234-204-8.ap-south-1.compute.amazonaws.com/',
     aws_endpoint: 'http://127.0.0.1:9000'
 };
